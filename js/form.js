b167acddd8f388bb0d4bb6d48175c02c6a5780b4
GabrielRom1 <gabriel.rome787@gmail.com>
2025-03-04 20:38:46 -0400

    cambios finales al form

--- js/form.js
+++ js/form.js
@@ -1,39 +1,45 @@
+let count = 0;
+
 document.getElementById("form").addEventListener("submit", function(){
-    let content = document.getElementById("submit-container");  
+    count = count +1;
 
-    let img = document.createElement("img");
+    // le digo al user que ha sometido mucho
+    if(count > 7){
+        alert("Porfavor no sigas sometiendo cosas inválidas...");
+    }
 
+    // capturo los valores de los inputs
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
 
+    let content = document.getElementById("submit-container");  
+    let img = document.createElement("img");
+
+
     if(!validForm(name, email, message) ){
-        img.src = "../static/logo-beca-BFC.jpg"
-        
-        img.alt = "imagen al someter";
-
-        img.style.width = "200px";
-
-        content.appendChild(img);
+        // imagen de error 
+        img.src = "../static/fail.jpg"
     }
     else{
-
-        img.src = "../download.jpeg" ;
-
-        img.alt = "imagen al someter";
-
-        img.style.width = "200px";
-
-        content.appendChild(img);
+        // succes submit
+        img.src = "../static/success.jpeg" ;
     }
+
+    img.alt = "imagen al someter";
+
+    img.style.width = "200px";
+
+    content.appendChild(img);
 });
 
 
-
+// verifico si un form es valido
 function validForm(name, email, message){
     return (validName(name) && validEmail(email) && validMessage(message)   );
 }
 
+// verifico si un nombre es valido (si no es vacio)
 function validName(name){
     if(name == ''){
         alert("Debes ingresar un nombre!");
@@ -43,68 +49,81 @@ function validName(name){
     return true;
 }
 
-
+// verifico si un email es valido
 function validEmail(email){
+    // caso si es vacio
     if(email == ''){
         alert("Debes ingresar un email!");
         return false;
     }
+    // si no tiene nada antes del @
     else if( !hasPrefix(email) ){
         return false;   
     }
-
+    // si no tiene nada valido despues del @
     else if(!hasPostFix(email) ){
         return false;
     }
     return true;
 }
 
+// verifico el prefix (lo de antes del @)
 function hasPrefix(email){
+    // primera aparicion del @
     let firstAtIndex = email.indexOf('@');
     if(firstAtIndex == -1){
         alert("El email ingresado no tiene @.");
         return false;
     }
+
+    // ultima aparicion del @
     let lastAtIndex = email.lastIndexOf('@');
 
+    // deben ser iguales sino el email tiene 2 @
     if(firstAtIndex != lastAtIndex){
         alert("El email ingresado tiene dos @. Es inválido.");
         return false;
     }
-    
+
+    //verifica si empieza con @ 
     if(firstAtIndex == 0){
         alert("El email ingresado empieza con @.");
         return false;
     }
-    let prefix = email.slice(0,firstAtIndex);
-    
-    console.log("Prefix");
-    console.log(prefix);
+    // debugg
+    // let prefix = email.slice(0,firstAtIndex);
+    // console.log("Prefix");
+    // console.log(prefix);
 
     return true;
 }
 
-
+// verifica si tiene algo valido despues del @
 function hasPostFix(email){
     let firstAtIndex = email.indexOf('@');
 
+    // caso donde termina con @
     if(firstAtIndex == email.length -1){
         alert("El email ingresado termina con @.");
         return false;
     }
 
+    // capturo el postfix
     let postfix = email.slice(firstAtIndex+1, email.length);
 
+    // verifica si el postfix es valido (algo.domain)
     if( !validPostFix(postfix) ){
         return false;
     }
 
-    console.log("Postfix");
-    console.log(postfix);
+    // debug
+    // console.log("Postfix");
+    // console.log(postfix);
 
     return true;
 }
 
+//verifica si el postfix es valido (algo.domain) 
 function validPostFix(postfix){
     let firstDotIndex = postfix.indexOf('.');
     
@@ -125,14 +144,15 @@ function validPostFix(postfix){
         return false;
     }
 
-    let domain = postfix.slice(firstDotIndex+1, postfix.length);
-
-    console.log("Domain");
-    console.log(domain);
+    // debug
+    // let domain = postfix.slice(firstDotIndex+1, postfix.length);
+    // console.log("Domain");
+    // console.log(domain);
 
     return true;
 }
 
+// verifica si un mensaje es valido (si no esta vacio)
 function validMessage(message){
     if(message == ''){
         alert("Debes ingresar un mensaje!");
@@ -140,11 +160,3 @@ function validMessage(message){
     }
     return true;
 }
-
-// referencia de metodos de string
-// https://www.w3schools.com/jsref/jsref_obj_string.asp
-
-
-
-// https://www.reddit.com/r/webdev/comments/182tyzu/how_to_prevent_page_refresh_when_auto_submitting/?rdt=57377
-// referencia de return false en onsubmit
